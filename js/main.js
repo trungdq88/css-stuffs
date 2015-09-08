d4e0e1d71e07bb47d0a7aa9e605cf7af0f084801
Dinh Quang Trung <trungdq88@gmail.com>
2015-09-08 20:57:48 +0700

    Switch between tabs

--- js/main.js
+++ js/main.js
@@ -2,7 +2,20 @@ var isMenuOpen = false;
 var toggleElements;
 var currentTab;
 
+function getElementPosition(array, anchor, element) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == element) {
+            return i < [].indexOf.call(array, anchor) ? 'left' : 'right';
+        }
+    }
+    return 'unknown';
+}
+
 $(function () {
+
+    var tabs = $('.tab-item').map(function (index, el) {
+        return el.getAttribute('href').replace('#', '');
+    });
 
     toggleElements = [
         $('#main'),
@@ -14,8 +27,27 @@ $(function () {
     ];
 
     $('#menu-overlay').click(toggleMenu);
-    $('#menu a').click(function () {
-        var link = $(this).attr('href');
+    $('.tab-item').click(function () {
+        var tabId = $(this).attr('href').replace('#', '');
+
+        // Show/hide tabs
+        // 1. Show clicked tabs
+        var $clickedTab = $('.item-holder[data-tab=' + tabId +']');
+        $clickedTab.removeClass('hide');
+        setTimeout(function () {
+            $clickedTab.removeClass('move-right').removeClass('move-left');
+        }, 100);
+
+        // 2. Move other tabs to their right position
+        $('.item-holder').each(function (index, el) {
+            var itemId = el.dataset.tab;
+            if (itemId != tabId) { // Don't move the clicked tab
+                $(el).addClass('move-' + getElementPosition(tabs, tabId, itemId));
+                setTimeout(function () {
+                    $(el).addClass('hide');
+                }, 300);
+            }
+        });
 
         // Move the underline bar
         $('#underline-bar').css({
@@ -23,11 +55,12 @@ $(function () {
             width: $(this).width() + 40
         });
 
-        if (currentTab && currentTab != link) {
-            // Retain the menu
-            currentTab = link;
+        // Decide to close the menu or switch to other tabs
+        if (currentTab && currentTab != tabId) {
+            // Move to other tabs
+            currentTab = tabId;
         } else {
-            currentTab = link;
+            currentTab = tabId;
             toggleMenu();
         }
 
@@ -48,7 +81,9 @@ function closeMenu() {
         element.removeClass('open-menu');
     });
     $('#menu-overlay').hide();
-    $('#underline-bar').css({width: 0});
+    $('#underline-bar').css({
+        width: 0
+    });
     currentTab = false;
 }
 
@@ -59,4 +94,16 @@ function toggleMenu() {
         closeMenu();
     }
     isMenuOpen = !isMenuOpen;
+}
+
+function moveTab() {
+    $('#holder-1').addClass('move-left');
+    setTimeout(function () {
+        $('#holder-1').addClass('hide');
+    }, 100);
+    $('#holder-2').removeClass('hide');
+    setTimeout(function () {
+        $('#holder-2').removeClass('move-right');
+    }, 100);
+
 }
